29ef279a72f744acac1d9e801327db3dde82b7c4
Vitor Lopes <vcoder00@gmail.com>
2025-08-05 15:21:05 -0300

    mais questoes

---
+++ atividades/positivoOuNegativo.js
@@ -0,0 +1,12 @@
+function ehPositivo(x) {
+    if (x > 0) {
+        return console.log('positivo');
+    }
+    else if (x === 0) {
+        return console.log('nulo');
+    }
+    else {
+        return console.log('negativo');
+    }
+}
+ehPositivo(X);
